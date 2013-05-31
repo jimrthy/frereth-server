72d2eaeab2f9b8d7dbfd4b142f1c780103e6a079
James Ashley <james@teamdaves.us>
2013-05-30 22:21:37 -0500

    Flesh out some of the really minor details...have an ECHO server that runs 1 thread per core, exiting right after it receives its first message. At least that's the theory...I doubt that it actually does anything at all.

--- src/frereth_server/core.clj
+++ src/frereth_server/core.clj
@@ -6,8 +6,13 @@
 (defn get-cpu-count 
   "How many CPUs are available?"
   []
-  ;; This seems like it really should just be a very simple java interop call.
-  (throw (RuntimeException. "I still have to figure out what it is")))
+  ;; Note that it's extremely overly simplistic to try to max out the
+  ;; threads for message processing, for most intents and purposes.
+  ;; At least,  it really seems that way from my current perspective.
+  ;; At the very worst, should use the min of this and a value specified in
+  ;; config.
+  ;; Whatever. This is a start.
+  (.availableProcessors (Runtime/getRuntime)))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -17,5 +22,10 @@
     (mq/with-context context thread-count
       (do
         ;; Note that config/*port* controls where to listen
-        (let [sockets (build-sockets)]
-          (do-stuff))))))
+        (with-open [socket (-> context
+                               (mq/socket mq/pull)
+                               (mq/bind (str "tcp://127.0.0.1:" config/*port*)))]
+          ;; Let's start with ECHO
+          (let [pull (future (mq/recv socket))]
+            (let [msg @pull]
+              (mq/send socket msg))))))))
