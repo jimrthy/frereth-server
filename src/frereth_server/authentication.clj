de3a77ffdf6a014bd7af9234268194f3ebe92a48
James Ashley <james@teamdaves.us>
2013-07-02 00:30:44 -0500

    How should the login handler really work?

--- src/frereth_server/authentication.clj
+++ src/frereth_server/authentication.clj
@@ -7,9 +7,28 @@
   ;; FIXME: Deal with multi-part messages
   (mq/recv-str s))
 
+(defn- dispatcher [msg]
+  "This is just screaming for something like cond.
+It seems pretty important to remember that this isn't
+exactly performance-critical code.
+But it sort-of is.
+*If* a server's successful, people will hammer it pretty
+much constantly after it boots.
+Might be nice to allow players to download new worlds in
+the background and switch over to them fairly seamlessly,
+ignoring the entire patch day BS. Actually, that'd be
+a delightful advantage of using freenet.
+With the downside of supporting old servers until clients
+switch to the new version. It seems like an interesting
+thought."
+  (if (string? msg)
+    msg
+    (if (seq msg)
+      (throw (RuntimeException. "Now things start to get interesting")))))
+
 (defmulti ^:private dispatch
   "Based on message, generate response"
-  identity)
+  dispatcher)
 
 (defmethod dispatch "dieDieDIE!!"
   [_]
