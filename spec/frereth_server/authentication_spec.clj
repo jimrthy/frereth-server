5abe160c5cb8b2007396393e34fc25a4448ae4d3
James Ashley <james@teamdaves.us>
2013-07-02 00:31:39 -0500

    Go ahead and :use speclj in those tests instead of just :require

--- spec/frereth_server/authentication_spec.clj
+++ spec/frereth_server/authentication_spec.clj
@@ -1,54 +1,66 @@
 (ns frereth-server.authentication-spec
-  (:require [speclj.core :as spec]
-            [frereth-server.authentication :as auth]
+  (:require [frereth-server.authentication :as auth]
             [frereth-server.config :as config]
             [frereth-server.system :as sys]
             [zguide.zhelpers :as mq])
+  (:use [speclj.core :as spec])
   (:gen-class))
 
 (let [world (atom nil)
       client (atom nil)]
 
-  (spec/describe "Validate authentication"
-                 (spec/before-all
-                  (println "Preparing")
-                  (reset! world (sys/init)))
+  (describe "Check authentication"
+            (before-all
+             (println "Preparing")
+             (reset! world (sys/init)))
 
-                 (spec/after-all
-                  (println "Ending")
-                  ;; Next line is causing an NPE. WTF?
-                  (if-let [client @client]
-                    (do (.close @client)
-                        (reset! client nil))
-                    (println "NULL client. Huh?"))
+            (after-all
+             (println "Ending")
+             (if-let [client @client]
+               (do (.close @client)
+                   (reset! client nil))
+               (println "NULL client. Huh?"))
 
-                  (reset! world nil))
+             (reset! world nil))
 
-                 (spec/before
-                  (println "\tTesting...")
-                  (swap! world sys/start)
-                  (reset! client (let [ctx (@world :network-context)
-                                       s (mq/socket @ctx mq/req)
-                                       address (format 
-                                                "tcp://127.0.0.1:%d"
-                                                config/auth-port)]
-                                   (mq/connect s address)
-                                   s)))
+            (before
+             (println "\tTesting...")
+             (swap! world sys/start)
+             (reset! client (let [ctx (@world :network-context)
+                                  s (mq/socket @ctx mq/req)
+                                  address (format 
+                                           "tcp://127.0.0.1:%d"
+                                           config/auth-port)]
+                              (mq/connect s address)
+                              s)))
 
-                 (spec/after
-                  (println "\t...Tested")
-                  (swap! world sys/stop))
+            (after
+             (println "\t...Tested")
+             (swap! world sys/stop))
 
-                 (spec/it "Kill"
-                          (spec/should= "K" (#'auth/dispatch "dieDieDIE!!")))
+            (it "Kill"
+                (should= "K" (#'auth/dispatch "dieDieDIE!!")))
 
-                 (spec/it "Kill Message"
-                          (reset! (:done @world) true)
-                          (let [auth-thread (:authentication-thread world)]
-                            (mq/send @client "dieDieDIE!!")
-                            (let [response (mq/recv-str @client)]
-                              (spec/should= "K" response)
-                              (.join @auth-thread 50)
-                              (spec/should (not (.isAlive @auth-thread))))))))
+            (it "Kill Message"
+                (reset! (:done @world) true)
+                (let [auth-thread (:authentication-thread world)]
+                  (mq/send @client "dieDieDIE!!")
+                  (let [response (mq/recv-str @client)]
+                    (should= "K" response)
+                    (.join @auth-thread 50)
+                    (should (not (.isAlive @auth-thread))))))
 
-(spec/run-specs)
+            (it "Basic Login Sequence"
+                ;; This is trickier than I realized.
+                ;; I really want to send a multi-part message.
+                ;; Something along these lines:
+                ;; 1) ib <user-id>
+                ;; 2) me-wantz-play <protocol versions client understands>
+                ;; 3) <signature>
+                ;; That approach seems like it will just make
+                ;; life simpler from any angle that comes to mind.
+                ;; Dispatcher gets more complicated,but that isn't
+                ;; a big deal.
+                (should false))))
+
+(run-specs)
