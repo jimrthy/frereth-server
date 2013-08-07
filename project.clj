d5042f8077722ecc6ac4c0fe4175800a35814000
James Ashley <james.ashley@newcomlink.com>
2013-08-06 22:26:37 -0500

    Remove extraneous dependencies (mostly comments) from the project

--- project.clj
+++ project.clj
@@ -6,15 +6,9 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.zeromq/jzmq "2.2.1"]
                  [org.zeromq/cljzmq "0.1.1"]]
-  ;;:jvm-opts ["-Djava.library.path=/usr/lib:/usr/local/lib"]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.3"]
                                   [org.clojure/java.classpath "0.2.0"]
-                                  [expectations "1.4.49"]
-                                  ;; Should go away
-                                  [speclj "2.5.0"]]}}
-  :plugins [[lein-expectations "0.0.7"]
-            ;; Should also go away
-            [speclj "2.5.0"]]
-  :test-paths ["spec/"]
+                                  [expectations "1.4.49"]]}}
+  :plugins [[lein-expectations "0.0.7"]]
   :main frereth-server.core)
