fa998cd128f0919177e52a0d5120d591037f8168
James Ashley <james.ashley@newcomlink.com>
2013-08-06 22:25:10 -0500

    Problem with unsatisfied link error came from needing the reference to jzmq (or jeromq...whichever strikes your fancy)

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Affero General Public License"
             :url "http://www.gnu.org/licenses/agpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 ;;[cc.qbits/jilch "0.3.0"]
-                 ;;[org.zeromq/jzmq "2.2.0"]
+                 [org.zeromq/jzmq "2.2.1"]
                  [org.zeromq/cljzmq "0.1.1"]]
+  ;;:jvm-opts ["-Djava.library.path=/usr/lib:/usr/local/lib"]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.3"]
                                   [org.clojure/java.classpath "0.2.0"]
