c4c501124010c0b30a20a77d0f1566c9393f47bc
James Gatannah <jgatannah@gmail.com>
2014-08-20 04:20:11 +0000

    Bump some project version #'s, though I'm sure it'll break things that it'll take me forever to find time to fix

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :url "http://frereth.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[byte-transforms "0.1.0"]
+  :dependencies [[byte-transforms "0.1.3"]
                  [com.postspectacular/rotor "0.1.0"]
-                 [com.taoensso/timbre "2.7.1"]
+                 [com.taoensso/timbre "3.2.1"]
+                 [im.chit/ribol "0.4.0"]
                  ;; For now, this next library needs to be distributed to
                  ;; a local maven repo.
                  ;; It seems like it should really take care of its handler
@@ -14,7 +15,7 @@
                  ;; it gets more complicated. Still, we shouldn't be worrying
                  ;; about details like jeromq vs jzmq here.
                  [org.clojars.jimrthy/cljeromq "0.1.0-SNAPSHOT"]
-                 [org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure "1.7.0-alpha1"]
                  ;; See if swapping to jeromq makes life easier.
                  ;; This seems like I'll be missing an important
                  ;; point when the security/encryption pieces fall
@@ -27,10 +28,10 @@
                  [org.zeromq/cljzmq "0.1.1" :exclusions [org.zeromq/jzmq]]]
   :main frereth-server.core
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[midje "1.6.0"]
-                                  [org.clojure/tools.namespace "0.2.3"]
-                                  [org.clojure/java.classpath "0.2.0"]
+                   :dependencies [[midje "1.6.3"]
+                                  [org.clojure/tools.namespace "0.2.5"]
+                                  [org.clojure/java.classpath "0.2.2"]
                                   [ritz/ritz-debugger "0.7.0"]]}}
-  :plugins [[lein-midje "3.0.0"]]
+  :plugins [[lein-midje "3.1.3"]]
   :repl-options {:init-ns user}
   :repositories {"sonatype-nexus-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"})
