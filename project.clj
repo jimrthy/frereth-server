4d7398dc65c7c8a72a01bf1cf11f986b7264fddc
James Gatannah <james.gatannah@gmail.com>
2014-11-28 11:49:53 -0600

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[byte-transforms "0.1.3"]
-                 [com.cemerick/pomegranate "0.3.0"]
+                 ;; Q: Does this make any sense in production?
+                 ;; A: Well, it makes sense for the general runtime which
+                 ;; is the primary goal.
+                 [com.cemerick/pomegranate "0.3.0" :exclusions [org.codehaus.plexus/plexus-utils]]
                  [com.postspectacular/rotor "0.1.0"]
                  [com.stuartsierra/component "0.2.2"]
                  [com.taoensso/timbre "3.3.1"]
@@ -19,11 +22,11 @@
                  ;;[org.clojars.jimrthy/cljeromq "0.1.0-SNAPSHOT"]
                  [org.clojure/clojure "1.7.0-alpha1"]
                  [org.zeromq/cljzmq "0.1.4"]
-                 [prismatic/schema "0.3.2"]]
+                 [prismatic/schema "0.3.3"]]
   :jvm-opts [~(str "-Djava.library.path=/usr/local/lib:" (System/getenv "LD_LIBRARY_PATH"))]
   :main frereth-server.core
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[midje "1.6.3"]
+                   :dependencies [[midje "1.6.3" :exclusions [joda-time]]
                                   [org.clojure/tools.namespace "0.2.7"]
                                   [org.clojure/java.classpath "0.2.2"]]}
              :uberjar {:aot :all}}
