f42d68a7631a8618bcddabfa958c7e3e0b54534b
James Gatannah <james.gatannah@gmail.com>
2015-05-28 22:21:49 -0500

    Add a plugin for building dockers
    This approach is almost guaranteed to fail because there really isn't a
    prayer that the default base container has jzmq installed

--- project.clj
+++ project.clj
@@ -30,6 +30,7 @@
                                   [org.clojure/tools.namespace "0.2.10"]
                                   [org.clojure/java.classpath "0.2.2"]]}
              :uberjar {:aot :all}}
-  :plugins [[lein-midje "3.1.3"]]
+  :plugins [[lein-midje "3.1.3"]
+            [com.palletops/uberimage "0.4.1"]]
   :repl-options {:init-ns user}
   :repositories {"sonatype-nexus-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"})
