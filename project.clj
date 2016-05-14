20761ebfb00824be69b4892a1191945038124f7a
James Gatannah <james.gatannah@gmail.com>
2016-05-13 22:16:51 -0500

    Main point is just marking that it's headless
    But also got rid of log rotation. The ancient version of timbre I build
    that part around has changed, it was wrong in the first place, and it
    doesn't belong here in any event.
    
    All part of the fun and games of dredging up ancient history.

--- project.clj
+++ project.clj
@@ -8,7 +8,10 @@
                  [com.datomic/datomic-free "0.9.5206" :exclusions [joda-time org.clojure/clojure]]
                  [com.frereth/common "0.0.1-SNAPSHOT"]
                  [com.postspectacular/rotor "0.1.0"]
+                 [com.stuartsierra/component "0.2.3"]
+                 [com.taoensso/timbre "3.4.0"]
                  [datomic-schema "1.3.0" :exclusions [org.clojure/clojure]]
+                 [im.chit/ribol "0.4.0"]
                  ;; TODO: This really doesn't belong in here
                  [io.rkn/conformity "0.3.5"]
                  ;; I'm not using these, but com.palletops/uberimage and lein-ancient
@@ -16,10 +19,21 @@
                  ;; are competing over older versions
                  [org.apache.httpcomponents/httpclient "4.4.1"]
                  [org.apache.httpcomponents/httpcore "4.4.1"]
-                 [org.apache.httpcomponents/httpmime "4.4.1"]]
-  ;; Q: Is there a good way to move this up into common?
+                 [org.apache.httpcomponents/httpmime "4.4.1"]
+                 ;; For now, this next library needs to be distributed to
+                 ;; a local maven repo.
+                 ;; It seems like it should really take care of its handler
+                 ;; ...except that very likely means native libraries, so
+                 ;; it gets more complicated. Still, we shouldn't be worrying
+                 ;; about details like jeromq vs jzmq here.
+                 ;;[org.clojars.jimrthy/cljeromq "0.1.0-SNAPSHOT"]
+                 [org.clojure/clojure "1.7.0-RC1"]
+                 [org.zeromq/cljzmq "0.1.4"]
+                 [prismatic/schema "0.4.3"]]
+  ;; Q: Is there a good way to move the extra library path up into common?
   ;; Better Q: Now that I've copied it into common, do I still need this here and in client?
-  :jvm-opts [~(str "-Djava.library.path=/usr/local/lib:" (System/getenv "LD_LIBRARY_PATH"))]
+  :jvm-opts [~(str "-Djava.library.path=/usr/local/lib:" (System/getenv "LD_LIBRARY_PATH"))
+             "-Djava.awt.headless=true"]
   :main frereth.server.core
   :profiles {:dev {:dependencies [[org.clojure/java.classpath "0.2.2"
                                    :exclusions [org.clojure/clojure]]]
--- src/com/frereth/server/logging.clj
+++ src/com/frereth/server/logging.clj
@@ -1,8 +1,6 @@
 (ns com.frereth.server.logging
   (require [com.stuartsierra.component :as component]
-           [taoensso.timbre :as log
-            :refer (trace debug info warn error fatal spy with-log-level)]
-           [taoensso.timbre.appenders.3rd-party.rotor :as rotor]))
+           [taoensso.timbre :as log]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Schema
@@ -13,9 +11,11 @@
   (start
     [this]
     (log/merge-config!
-     {:appenders {:rotor (rotor/rotor-appender {:path "logs/app.log"
-                                                :max-size (* 512 1024)
-                                                :backlog 5})}})
+     ;; Q: What happened to log rotator?
+     ;; A: Doesn't matter.
+     {:appenders {:rotor (comment (rotor/rotor-appender {:path "logs/app.log"
+                                                         :max-size (* 512 1024)
+                                                         :backlog 5}))}})
     (log/warn "FIXME: Log to a database instead")
     this)
 
