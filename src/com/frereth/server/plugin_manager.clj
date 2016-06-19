19b3fa792d42b65d972056ccff2636a80131ddff
James Gatannah <james.gatannah@gmail.com>
2016-06-19 13:24:30 -0500

    Couple more baby steps towards loading an App

--- src/com/frereth/server/plugin_manager.clj
+++ src/com/frereth/server/plugin_manager.clj
@@ -4,7 +4,9 @@
 For starters, everything's going to be an App. I'll almost definitely
 want to add Daemons (that's really what getty is, right?), but start
   with this approach"
-  (:require [com.frereth.common.schema :as frereth-schema]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [com.frereth.common.schema :as frereth-schema]
             [com.frereth.system.sentinal :as sentinal]
             [com.stuartsierra.component :as cpt]
             [schema.core :as s])
@@ -40,8 +42,21 @@ want to add Daemons (that's really what getty is, right?), but start
 (s/defn load-plugin :- EventPair
   [this :- PluginManager
    path :- [s/Symbol]]
-  ;; Note that we really need a callback for when the App exits
-  (throw (ex-info "Start Here" {})))
+  (let [absolute-path (into [(:root-namespace this)] path)
+        path-names (map str absolute-path)
+        ;; Q: What's the equivalent for python's os.path_separator?
+        resource-path (clojure.string/join "/" path-names)]
+    ;; Q: Would this be worth putting in a database instead?
+    (if-let [url (io/resource resource-path)]
+      ;; Note that we really need a callback for when the App exits
+      (with-open [rdr (io/reader url)]
+        (let [source-code (edn/read rdr)]
+          ;; Note that this is really where sandboxes start to come
+          ;; into play. Maybe something like OSGi or even clojail
+          ;; (that's what convinced me to take a serious look it
+          ;; clojure in the first place, after all).
+          (throw (ex-info "Start Here" {}))))
+      (throw (ex-info "Missing App" {:path resource-path})))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Public
